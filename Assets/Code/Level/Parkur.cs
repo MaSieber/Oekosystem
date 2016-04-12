6ba7584e4001df72e954891ee6b07367bf4b62fd
Marco Sieber <marco.sieber@games-academy.de>
2016-04-12 18:26:35 +0200

    Added first Prototype Stuff

---
+++ Assets/Code/Level/Parkur.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parkur : MonoBehaviour {
+
+    //Inspector Settings
+    public float fadeOutModifier = 1.0f;
+    enum Events
+    {
+        None = 0,
+        EnergySuckOut = 1
+    }
+    private Events eventType;
+
+	// Use this for initialization
+	void Start () {
+        eventType = Events.None;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float deltaTime = Time.deltaTime;
+
+        switch (eventType)
+        {
+            case Events.EnergySuckOut:
+                Color col = this.gameObject.GetComponent<SpriteRenderer>().material.GetColor("Tint");
+                col.a -= deltaTime * fadeOutModifier;
+
+                if (col.a < 0.0f)
+                { 
+                    col.a = 0.0f;
+                    eventType = Events.None;
+                }
+
+                break;
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            eventType = Events.EnergySuckOut;
+        }
+    }
+
+
+}
---
+++ Assets/Code/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float PlayerSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float deltaTime = Time.deltaTime;
+
+        Vector3 movement = Vector3.zero;
+        if (Input.GetButton("Horizontal"))
+        {
+            movement.x += PlayerSpeed * deltaTime;
+        }
+        
+        this.gameObject.transform.Translate(movement);
+    }
+}
