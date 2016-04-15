b285d55a60e87491d5d27822ace93193e004e116
Marco Sieber <marco.sieber@games-academy.de>
2016-04-15 02:10:29 +0200

    Improved Hover Platform Behavior (Scale + Pos get Calculated too) Fixed some Hover Platform which were a bit offset

--- Assets/Code/LevelElement/HoverPlatform.cs
+++ Assets/Code/LevelElement/HoverPlatform.cs
@@ -16,11 +16,15 @@ public class HoverPlatform : MonoBehaviour {
     private int direction = 1;
 
     private Vector2 Size;
+    private Vector3 Scale;
+    private Vector3 Pos;
+
 	// Use this for initialization
 	void Start () {
         currentRadiant = 0.0f;
         Size = transform.parent.FindChild("Stange").gameObject.GetComponent<BoxCollider2D>().size;
-        
+        Scale = transform.parent.FindChild("Stange").gameObject.transform.localScale;
+        Pos = transform.parent.FindChild("Stange").gameObject.transform.localPosition;
     }
 	
 	// Update is called once per frame
@@ -42,11 +46,13 @@ public class HoverPlatform : MonoBehaviour {
 
         if (platformType == PlatformType.Vertical)
         {
-            posY = Mathf.Lerp(-Size.y/2, Size.y/2, transition);
+            float sizeY = Pos.y + Size.y * Scale.y;
+            posY = Mathf.Lerp(-sizeY/2, sizeY/2, transition);
         }
         else if (platformType == PlatformType.Horizontal)
         {
-            posX = Mathf.Lerp(-Size.x, Size.x, transition);
+            float sizeX = Pos.x + Size.x * Scale.x;
+            posX = Mathf.Lerp(-sizeX/2, sizeX/2, transition);
         }
 
         transform.localPosition = new Vector3(posX,posY,0.0f);
